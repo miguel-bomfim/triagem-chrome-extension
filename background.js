e27e71709db3810fbc7b0d69ca0fdfa6cca00308
Miguel Bomfim <60234561+miguel-bomfim@users.noreply.github.com>
2025-01-29 17:28:08 -0300

    Update background.js

--- background.js
+++ background.js
@@ -1,19 +1,34 @@
-let extractedData = {};
+// Get current user from mgraneis
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	if(message.action === 'updateHref') {
-		extractedData = message.data
-		chrome.storage.local.set({ extractedData });
-	}
-});
+      // Check if the request is the login request
+      if (details.method === "POST" && details.requestBody.formData) {  // Replace with the actual login URL
+        // Decode the request body (FormData)
+        const formData = details.requestBody.formData
+        const username = formData.login_usuario[0];
 
-
-chrome.tabs.onActivated.addListener((activeInfo) => {
- chrome.tabs.sendMessage(activeInfo.tabId, { action: 'updateData' }); 
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
- if (changeInfo.status === 'complete') {
- chrome.tabs.sendMessage(tabId, { action: 'updateData' });    
-  }
-});
+        function formatUsername(user) {
+          // Split the username into parts
+          const parts = user.split('.');
+        
+          // Capitalize the first letter of each part and make the rest lowercase
+          const formattedParts = parts.map(part => {
+            return part.charAt(0).toUpperCase() + part.slice(1).toLowerCase();
+          });
+        
+          // Join the parts with a space
+          return formattedParts.join(' ');
+        }
+  
+        // Save the username to Chrome storage
+        if (username) {
+          chrome.storage.local.set({ username: formatUsername(username) }, function () {
+            console.log("Username saved:", formatUsername(username));
+          });
+        }
+      }
+    },
+    { urls: ["https://www.mgraneis.com.br/*"] },  // Replace with the actual website URL
+    ["requestBody"]
+  );
